302228187bb462d394b509aee6954dd4088459e3
Kevin Mueller <kevin@idahomuellers.com>
2023-09-24 04:05:54 +0000

    #11 tf provider s3

--- main.tf
+++ main.tf
@@ -5,16 +5,34 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
+provider "aws" {
+  # Configuration options
+}
 provider "random" {
   # Configuration options
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
+  lower = true
+  upper = false
+  length  = 32
+  special = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+
+#  tags = {
+#    Name        = "My bucket"
+#    Environment = "Dev"
+#  }
 }
 
 output "random_bucket_name" {
