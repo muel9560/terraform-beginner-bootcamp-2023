15f127c9e4006dcb742134b1aa44a6528c42f456
Kevin Mueller <kevin@idahomuellers.com>
2023-09-30 21:49:49 -0600

    17-updates

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-
-terraform {
-  cloud {
-    organization = "Idaho-Muellers"
-    workspaces {
-      name = "terraform-beginner-bootcamp-2023"
-    }
-  }
-  
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   lower = true
   upper = false
@@ -36,12 +8,9 @@ resource "random_string" "bucket_name" {
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
 
-#  tags = {
-#    Name        = "My bucket"
-#    Environment = "Dev"
-#  }
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
 
-output "random_bucket_name" {
-    value = random_string.bucket_name.result
-}
+
