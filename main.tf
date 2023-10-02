76a2614196d1ec9d15bb9173e516eb6f758ae949
Kevin Mueller <kevin@idahomuellers.com>
2023-10-01 18:58:46 -0600

    21 terrahouse module

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    UserUuid = var.user_uuid
-  }
+terraform {
+#  cloud {
+#    organization = "Idaho-Muellers"
+#    workspaces {
+#      name = "terraform-beginner-bootcamp-2023"
+#    }
+#  }
 }
 
-
+module "terrahouse_aws"{
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
---
+++ modules/terrahouse_aws/variables.tf
@@ -0,0 +1,19 @@
+variable "user_uuid" {
+  description = "User UUID"
+  type        = string
+
+  validation {
+    condition = can(regex("^([a-f0-9]{8}-[a-f0-9]{4}-[4][a-f0-9]{3}-[89ab][a-f0-9]{3}-[a-f0-9]{12})$", var.user_uuid))
+    error_message = "User UUID must be in the format of a UUID (e.g., 123e4567-e89b-12d3-a456-426614174000)"
+  }
+}
+
+variable "bucket_name" {
+  description = "AWS S3 Bucket Name"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name))
+    error_message = "Invalid bucket name. Bucket names must be lowercase, between 3 and 63 characters, and can only contain lowercase letters, numbers, hyphens, and periods (dots)."
+  }
+}
