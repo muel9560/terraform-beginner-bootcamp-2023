c24329180087813c88aa7602323b430ebaacf44d
Kevin Mueller <87453797+muel9560@users.noreply.github.com>
2023-10-01 19:08:22 -0600

    21 aws terrahouse module (#22)
    * 21 terrahouse module
    
    * 21 fixing empty provider

---
+++ modules/terrahouse_aws/main.tf
@@ -0,0 +1,23 @@
+terraform {
+#  cloud {
+#    organization = "Idaho-Muellers"
+#    workspaces {
+#      name = "terraform-beginner-bootcamp-2023"
+#    }
+#  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
+}
+
--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,4 +1,4 @@
 output "bucket_name" {
     description = "Bucket name for our static website"
-    value = module.terrahouse_aws.bucket_name
+    value = aws_s3_bucket.website_bucket.bucket
 }
