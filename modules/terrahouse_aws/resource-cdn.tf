f5694fe050a5ca95000b950044b1970415d33355
Kevin Mueller <kevin@idahomuellers.com>
2023-10-03 21:47:31 -0600

    25 fixing tf

--- modules/terrahouse_aws/resource-cdn.tf
+++ modules/terrahouse_aws/resource-cdn.tf
@@ -12,7 +12,7 @@ resource "aws_cloudfront_origin_access_control" "default" {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name              = aws_s3_bucket.b.bucket_regional_domain_name
+    domain_name              = aws_s3_bucket.website_bucket.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.default.id
     origin_id                = local.s3_origin_id
   }
--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -40,18 +40,20 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.bucket
   #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
   policy = jsonencode(
-    "Version"= "2012-10-17",
-    "Statement" = {
-        "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
-        "Effect" = "Allow",
-        "Principal" = {
-            "Service" = "cloudfront.amazonaws.com"
-        },
-        "Action" = "s3:GetObject",
-        "Resource" = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
-        "Condition" = {
-            "StringEquals" = {
-                "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+    {
+        "Version" = "2012-10-17",
+        "Statement" = {
+            "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
+            "Effect" = "Allow",
+            "Principal" = {
+                "Service" = "cloudfront.amazonaws.com"
+            },
+            "Action" = "s3:GetObject",
+            "Resource" = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
+            "Condition" = {
+                "StringEquals" = {
+                    "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
+                }
             }
         }
     }
