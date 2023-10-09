7bbc63c9cc26d0ec5764b9db51ccfa4a2baa48e3
Kevin Mueller <kevin@idahomuellers.com>
2023-10-08 21:01:37 -0600

    31 assets upload

--- main.tf
+++ main.tf
@@ -7,11 +7,12 @@ terraform {
 #  }
 }
 
-module "terrahouse_aws"{
+module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
+  assets_path = var.assets_path
   content_version = var.content_version
 }
