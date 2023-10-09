c56692bb24616d121e912546cb1d5a664791adba
Kevin Mueller <kevin@idahomuellers.com>
2023-10-08 20:50:57 -0600

    31 asset upload

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -18,6 +18,18 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
   }
 }
 
+resource "aws_s3_object" "upload_assets" {
+  for_each = fileset("${path.root}/public/assets", "*.jpg,png,gif")
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key = "assets/${each.key}"
+  source = "${path.root}/public/assets/${each.key}"
+  etag = filemd5("${path.root}/public/assets/${each.key}")
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
+}
+
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key = "index.html"
