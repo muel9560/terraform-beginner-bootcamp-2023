0d2dbdb248f6304343d5f86ef9a822bfa6452783
Kevin Mueller <kevin@idahomuellers.com>
2023-10-12 22:02:21 -0600

    43 finishing provider

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -27,3 +27,14 @@ provider "terratowns" {
 #  assets_path = var.assets_path
 #  content_version = var.content_version
 #}
+
+resource "terratown_home" "home" {
+  name = "How to"
+  description = <<DESCRIPTION
+How to description
+DESCRIPTION
+#  domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "fnjsijrvis.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
